dc0beadea18eea5f913a4b5658e14c8a8cd2a9c9
Cameron Nepe <cameron.nepe@gmail.com>
2020-06-18 02:57:36 +0000

    learning to use variables

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 print('Hello World!')
-print(1 + 2)
+print(1 * 2)
+
+'''
+
+Playing with some variables
+
+'''
+x = 10
+y = 200
+print(x * y)
+
+oneHundread = 100
+twoThousand = 2000
+
+print(oneHundread * twoThousand)
+
